4c0cca0a5a0f3b71d488e96f17d669ac10607498
Siphamandla Bili <220047891@student.uj.ac.za>
2022-12-26 19:57:37 +0200

    ,

--- starter/script.js
+++ starter/script.js
@@ -5,22 +5,20 @@ const overlay = document.getElementsByClassName('.overlay');
 const btnModal = document.querySelector('.close-modal');
 const openModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0 ; i < openModal.length ; i ++) {
-    openModal[i].addEventListener('click',function(){
+for (let i = 0; i < openModal.length; i++) {
+    openModal[i].addEventListener('click', function () {
         modal.classList.remove('hidden');
     })
 }
 
-btnModal.addEventListener('click', function(){
+btnModal.addEventListener('click', function () {
     modal.classList.add('hidden');
 })
 
-document.addEventListener('keydown',function(event){
-   
-    if (event.key == 'Escape' && (!modal.classList.contains('hidden'))){
-        
+document.addEventListener('keydown', function (event) {
+
+    if (event['key'] === 'Escape' && (!modal.classList.contains('hidden'))) {
         modal.classList.add('hidden');
-        alert('')
     }
-    
+
 })
